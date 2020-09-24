56f54629660ab3b1a88bd9cd64fd389d4cba610c
Jesper Lundborg <jesper.lundborg@student.kyh.se>
2020-09-24 17:40:39 +0200

    testar om det funkar nu!

---
+++ src/com/company/Patient.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Patient {
+    String name;
+    String sickness;
+
+    Patient(String name, String sickness) {
+        this.name = name;
+        this.sickness = sickness;
+    }
+
+    protected Patient(String name) {
+        this.name = name;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected boolean isSick() {
+        return sickness != null;
+    }
+
+    protected String getSickness() {
+        return sickness;
+    }
+
+    protected void takeMedication(Medicine medicine) {
+        if (medicine.getTreatmentName().equals(this.sickness)) {
+            this.sickness = null;
+        }
+    }
+
+}
